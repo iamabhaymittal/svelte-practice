1d3eb15d3b9739b05c82ec92838e257e5a21cd7e
Abhay Mittal <iamabhaymittal@gmail.com>
2023-01-12 15:15:42 +0530

    conditionals in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,38 @@
 <script>
   import Face from "./Face.svelte";
-  let say = 'hi';
+  import Container from "./Container.svelte";
+  let say = false;
+  setTimeout(() => {
+    say = true;
+  }, 2000)
 </script>
+
+<Container>
+  {#if say}
+    <div>
+      Hi!
+    </div>
+  {:else if !say}
+    not saying anything....
+  {/if}
+  {#each [2,1,0] as index }
+  <Face {index}  />
+    
+  {/each}
+  
+
+</Container>
+
 <style>
   div {
     color: red;
   }
-  :global(div){
-    background-color: blue;
+  :global(*){
+    box-sizing: border-box;
+  }
+  :global(body,html) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
   }
 </style>
-<div>
-  Say: {say}
-</div>
-<Face size="4"/>
-<Face size="14"/>
-<Face />
--- src/Face.svelte
+++ src/Face.svelte
@@ -1,4 +1,6 @@
 <script>
     export let size = "1";
+    export let index = 3 ;
+    let faces = [":)",":(","=)","=("]
 </script>
-<div style="font-size: {size}em;">=}</div>
+<div style="font-size: {size}em;">{faces[index]}</div>
